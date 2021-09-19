67f4a7723df124de7851d71bdec2d0e37dd0313b
Mike Zeplichal <mike.zeplichal@hotmail.com>
2021-09-19 23:48:40 +0200

    Refactored newGame and added undoLastDart Function

--- js/dartcounter.js
+++ js/dartcounter.js
@@ -4,8 +4,14 @@ var playerThree = 501;
 var playerFour = 501;
 var p1 = true;
 var p2 = false;
+var doubleInPlayerOne;
+var doubleInPlayerTwo;
+var hitDoubleInPlayerOne;
+var hitDoubleInPlayerTwo;
 var counterPlayerOne = 0;
 var counterPlayerTwo = 0;
+var lastDartPlayerOne;
+var lastDartPlayerTwo;
 var threeDartsPlayerOne = [];
 var threeDartsPlayerTwo = [];
 var interval;
@@ -13,6 +19,42 @@ var interval;
 function getPlayers(players) {
   return players;
 }
+
+document.querySelector("#nextgame").onclick = function () {
+  newGame();
+};
+function newGame() {
+  playerOne = 501;
+  playerTwo = 501;
+  threeDartsPlayerOne = [];
+  threeDartsPlayerTwo = [];
+  counterPlayerOne = 0;
+  counterPlayerTwo = 0;
+  document.getElementById("count1").innerHTML = playerOne;
+  document.getElementById("count2").innerHTML = playerTwo;
+}
+
+document.querySelector("#undoDarts").onclick = function () {
+  undoLastDart();
+};
+function undoLastDart() {
+  if (p1 === true) {
+    playerOne += lastDartPlayerOne;
+    counterPlayerOne -= 1;
+    document.getElementById("count1").innerHTML = playerOne;
+    threeDartsPlayerOne.pop();
+    document.querySelector("#dartsPlayerOne").innerHTML = threeDartsPlayerOne;
+  } else {
+    playerTwo += lastDartPlayerTwo;
+    counterPlayerTwo -= 1;
+    document.getElementById("count1").innerHTML = playerOne;
+    document.getElementById("count2").innerHTML = playerTwo;
+    threeDartsPlayerTwo.pop();
+    document.querySelector("#dartsPlayerTwo").innerHTML = threeDartsPlayerTwo;
+  }
+}
+
+function checkDoubleIn(doubleIn, hitDoubleIn) {}
 
 function changePlayer(player) {
   if (player === 1) {
@@ -31,10 +73,20 @@ function changePlayerButton() {
     p1 = false;
     p2 = true;
     changePlayerColor();
+    threeDartsPlayerOne = [];
+    counterPlayerOne = 0;
+    document.querySelector("#dartsPlayerOne").innerHTML = threeDartsPlayerOne;
+    document.querySelector("#nextPlayer").innerText =
+      "Next player is Player Two";
   } else {
     p1 = true;
     p2 = false;
     changePlayerColor();
+    threeDartsPlayerTwo = [];
+    counterPlayerTwo = 0;
+    document.querySelector("#dartsPlayerTwo").innerHTML = threeDartsPlayerTwo;
+    document.querySelector("#nextPlayer").innerText =
+      "Next player is Player One";
   }
 }
 
@@ -49,33 +101,29 @@ function addThrowToList(dart, player) {
   }
 }
 
-function checkWin(points) {
-  if (points === 0) {
-    document.querySelector("#nextPlayer").innerText = "";
-    const winner = document.createElement("h1");
-    winner.innerText = player + " has won the game!";
-    document.body.append(winner);
+function checkWin(points, player) {
+  if (points == 0) {
+    document.querySelector("#nextPlayer").innerText = player + " has won";
     playerOne = 501;
     playerTwo = 501;
   }
 }
 
 function checkZeroPoints(points) {
-  if ((points = 0)) {
+  if (points == 0) {
     return true;
   }
 }
 document.getElementById("p1").style.color = "blue";
-setCounter();
 
 function changePlayerColor() {
   if (p1 === true) {
-    console.log("colorchange");
     document.getElementById("p1").style.color = "blue";
-    document.getElementById("p2").style.color = "black";
+    document.getElementById("p2").style.color = "white";
   }
   if (p2 === true) {
-    document.getElementById("p1").style.color = "black";
+    document.getElementById("p1").style.fontSize = 100;
+    document.getElementById("p1").style.color = "white";
     document.getElementById("p2").style.color = "blue";
   }
 }
@@ -87,6 +135,7 @@ function changePoints(num) {
     document.querySelector("#dartsPlayerTwo").innerHTML = "";
     threeDartsPlayerTwo = [];
     addThrowToList(num, 1);
+    lastDartPlayerOne = num;
     if (playerOne < num) {
       changePlayer(1);
       document.querySelector("#nextPlayer").innerText =
@@ -107,7 +156,7 @@ function changePoints(num) {
     document.querySelector("#dartsPlayerOne").innerHTML = "";
     threeDartsPlayerOne = [];
     addThrowToList(num, 2);
-
+    lastDartPlayerTwo = num;
     if (playerTwo < num) {
       changePlayer(2);
       document.querySelector("#nextPlayer").innerText =
@@ -118,8 +167,8 @@ function changePoints(num) {
     playerTwo -= num;
     counterPlayerTwo++;
     if (checkZeroPoints(playerTwo)) {
-      checkWin(playerTwo, "Player One");
-      //playerOne += num;
+      console.log("HELP");
+      checkWin(playerTwo, "Player Two");
     }
   }
 
@@ -127,14 +176,14 @@ function changePoints(num) {
     changePlayer(1);
     document.querySelector("#nextPlayer").innerText =
       "Next player is Player Two";
-
+    lastDartPlayerOne = 0;
     counterPlayerOne = 0;
-    console.log(p1);
   }
   if (counterPlayerTwo == 3) {
     changePlayer(2);
     document.querySelector("#nextPlayer").innerText =
       "Next player is Player One";
+    lastDartPlayerTwo = 0;
     counterPlayerTwo = 0;
   }
   document.getElementById("count1").innerHTML = playerOne;
@@ -143,21 +192,6 @@ function changePoints(num) {
 
 document.querySelector("#nextPlayerButton").onclick = function () {
   changePlayerButton();
-};
-function setPointsBack() {
-  playerOne = 501;
-  playerTwo = 501;
-  document.getElementById("count1").innerHTML = playerOne;
-  document.getElementById("count2").innerHTML = playerTwo;
-}
-
-function setCounter() {
-  document.getElementById("counterPlayerOne").innerHTML = counterPlayerOne;
-  document.getElementById("counterPlayerTwo").innerHTML = counterPlayerTwo;
-}
-
-document.querySelector("#nextgame").onclick = function () {
-  setPointsBack();
 };
 
 document.querySelector("[id='1']").onclick = function () {
